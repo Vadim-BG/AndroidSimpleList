5f17678a7ca2ab73608b12ac4e334ec05b1fab1e
Vadim-BG <infotourbg.vadim@gmail.com>
2020-04-16 15:08:26 +0300

    Создание простого списка для Android

---
+++ app/src/main/java/com/example/simplelist/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.simplelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView plList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+
+        String[] names = {"Java", "Python", "Ruby", "JavaScript", "Matlab", "C", "C++", "Swift",
+                "Objective C", "Haskell", "Lisp", "Rust", "asdf"};
+
+        plList.setText("");
+        for(String name : names){
+            plList.append(name + "\n");
+        }
+    }
+}
